87ad37cab1d864560b855b6b07903322e96a17fa
hmzq-404 <66130102+hmzq-404@users.noreply.github.com>
2023-12-24 16:08:44 +0500

    Display books dynamically from library array

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+const bookContainer = document.querySelector(".books-container");
+const addBookButton = document.querySelector(".add-book");
+
 const myLibrary = [
     new Book("Hobbit", "Tolkien", 262, true),
     new Book("Harry Potter", "Rowling", 354, false)
 ];
+
 
 function Book(title, author, numberOfPages, read) {
     this.title = title;
@@ -9,6 +13,55 @@ function Book(title, author, numberOfPages, read) {
     this.numberOfPages = numberOfPages;
     this.read = read;
 }
+
+
+function displayBook(book) {
+    // Create outer elements
+    let bookElement = document.createElement("div");
+    bookElement.className = "book";
+    let bookInfoContainer = document.createElement("div");
+    bookInfoContainer.className = "book-info-container";
+    let bookButtonsContainer = document.createElement("div");
+    bookButtonsContainer.className = "book-buttons-container";
+
+    // Create book info fields
+    let bookTitle = document.createElement("h2");
+    bookTitle.className = "title";
+    bookTitle.textContent = book.title;
+
+    let bookAuthor = document.createElement("p");
+    bookAuthor.className = "author";
+    bookAuthor.textContent = `Author: ${book.author}`;
+
+    let bookNumberOfPages = document.createElement("p");
+    bookNumberOfPages.className = "number-of-pages";
+    bookNumberOfPages.textContent = `Number of Pages: ${book.numberOfPages}`;
+
+    let bookStatus = document.createElement("p");
+    bookStatus.className = "status";
+    bookStatus.textContent = "Status: ";
+    bookStatus.textContent += book.read ? "Read" : "Not Read";
+
+    // Add book info fields to their container
+    bookInfoContainer.append(bookTitle, bookAuthor, bookNumberOfPages, bookStatus);
+
+    // Create book buttons
+    let deleteButton = document.createElement("button");
+    deleteButton.className = "delete";
+    deleteButton.textContent = "Delete";
+
+    let toggleStatusButton = document.createElement("button");
+    toggleStatusButton.className = "toggle-status";
+    toggleStatusButton.textContent = "Toggle Status";
+
+    // Add book buttons to their container
+    bookButtonsContainer.append(deleteButton, toggleStatusButton);
+
+    // Add outer elements to DOM
+    bookContainer.append(bookElement);
+    bookElement.append(bookInfoContainer, bookButtonsContainer);
+}
+
 
 function addBookToLibrary() {
     let title = prompt("Enter title");
@@ -18,4 +71,7 @@ function addBookToLibrary() {
     let newBook = new Book(title, author, numberOfPages, read);
     myLibrary.push(newBook);
 }
+
+
+myLibrary.forEach(displayBook);
 
